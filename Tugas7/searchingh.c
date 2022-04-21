2229d1911ddcf02e26c0c210d5abdc6d605efdcc
Aghniafyaa <100354524+Aghniafyaa@users.noreply.github.com>
2022-04-21 21:51:19 +0700

    PRAKTIKUM 7
    Aghnia alifya kamila - 24060121140148

---
+++ Tugas7/searchingh.c
@@ -0,0 +1,11 @@
+/* Nama: aghnia alifya kamila
+NIM: 24060121140148 */
+
+#include <stdbool.h> 
+#ifndef SEARCHING_H_INCLUDED 
+#define SEARCHING_H_INCLUDED 
+
+void SEQSearchX1(int T[], int N, int *IX, int X); 
+void SEQSearchX2(int T[], int N, int X, int *IX, bool *Found); 
+
+#endif // SEARCHING_H_INCLUDED
