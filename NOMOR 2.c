d7472e6e4cb93403ca2b4059ea8b214438c52cdf
Aghniafyaa <100354524+Aghniafyaa@users.noreply.github.com>
2022-05-28 21:40:29 +0700

    PRAKTIKUM 5
    AGHNIA ALIFYA KAMILA -- 24060121140148

---
+++ NOMOR 2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // Kamus
+    int N, i, T[N], max, min;
+
+    // Algoritma
+    printf("Masukkan banyak jalur lintasan : ");
+    scanf("%d", &N);
+    for(i=0; i<N; i++){
+        printf("\n");
+        printf("Masukkan nilai jalur ke- %d : ", i+1);
+        scanf("%d", &T[i]);
+    }
+
+    max = T[0];
+    min = T[0];
+    for(i=0; i<N; i++) if(T[i]>max) max = T[i];
+    for(i=0; i<N; i++) if(T[i]<min) min = T[i];
+
+    printf("Selisih jalur terbesar dan terkecil : %d", max-min);
+    return 0;
+}
